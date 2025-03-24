f7477a89c10def5a755d21aae3417905f7df7a15
Marcos-Ordo <ordonezmarcos935@gmail.com>
2025-03-23 21:54:52 -0300

    re hice get_all con la idea de go_to. Ahora existe go_to_next_page

--- Maximus.py
+++ Maximus.py
@@ -5,36 +5,31 @@
 import Scraper
 import csv
 
+MICROS = "https://www.maximus.com.ar/Productos/Microprocesadores/maximus.aspx?/CAT=52/SCAT=-1/M=-1/OR=1/PAGE=1/"
+
 '''
-Interfaz: get_data(self); get_all(self); redo_data(self); search_for(self,search)
+Interfaz: get_all(self); redo_data(self); search_for(self,search)
 '''
 
 class Maximus(Scraper.Scraper):
     def __init__(self):
-        super().__init__("https://www.maximus.com.ar/", True)
+        super().__init__("https://www.maximus.com.ar/", False)
+        # Genero el csv donde guardar la información más tarde.
         with open('data.csv', mode='a', newline='') as output_file:
             data_csv = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             data_csv.writerow(['Maximus', ''])
             data_csv.writerow(['Artículo', 'Precio'])
-
-    # PROPÓSITO: Recibir la información de @page y guardar esa información en @product_dict.
-    # COND: @page debe tener una página.
-    def get_data(self):
-        super().get_content("product", "title-prod", "price")
+        # Guardo en page_data la información que voy a utilizar.
+        self.page_data = [
+            "product", "div", # [0,1] = Box
+            "title-prod", "span", # [2,3] = Description
+            "price", "div" # [4,5] = Price
+        ]
 
     # PROPÓSITO: Recibir toda la información relevante de la solapa donde @page está posicionada. 
     # COND: @page debe tener una página.
     def get_all(self):
-        # Preparo el while loop
-        i = 1
-        super().get_content("product", "title-prod", "price")
-        checker = super().check_if_data_exists("product")
-        while checker:
-            i += 1
-            site = self.actual_site[:self.actual_site.rindex("/")-1]+str(i)+"/" # Nasty!
-            self.reload_on(site)
-            checker = super().check_if_data_exists("product")
-            super().get_content("product", "title-prod", "price")
+        super().get_all('//div[@class="button-pager"]/li[last()-1]','//div[@class="col-md-9"]/div[@class="row"]/div[last()]')
 
     # PROPÓSITO: borra los datos que ya están en el csv y vuelve a escribir los titulos de la sección.
     def redo_data(self):
@@ -48,17 +43,13 @@ class Maximus(Scraper.Scraper):
     def search_for(self, search):
         super().search_for(search,"search-input")
 
-    ### IDEA; go_to(): moverme con los botones en la página; pienso que de esa forma no estoy cargando cada página desde 0.
-
-
 ## TESTS ##
-
 
 def test2():
     # OBJETIVO: Testear "get_all", la busqueda en dos paginas diferentes y el guardado de datos.
     m = Maximus()
     input("Press Enter to continue...")
-    m.reload_on("https://www.maximus.com.ar/Productos/Microprocesadores/maximus.aspx?/CAT=52/SCAT=-1/M=-1/OR=1/PAGE=1/")
+    m.reload_on(MICROS)
     input("Press Enter to continue...")
     m.get_all()
     input("Press Enter to continue...")
@@ -66,7 +57,7 @@ def test2():
     input("Press Enter to continue...")
     m.search_for("rtx")
     input("Press Enter to continue...")
-    m.get_data()
+    m.get_content()
     input("Press Enter to continue...")
     m.show_data()
     input("Press Enter to continue...")
@@ -78,15 +69,15 @@ def test1():
     # OBJETIVO: Testear la busqueda en dos páginas diferentes.
     m = Maximus()
     input("Press Enter to continue...")
-    m.reload_on("Micros")
+    m.reload_on(MICROS)
     input("Press Enter to continue...")
-    m.get_data()
+    m.get_content()
     input("Press Enter to continue...")
     m.show_data()
     input("Press Enter to continue...")
     m.search_for("rtx")
     input("Press Enter to continue...")
-    m.get_data()
+    m.get_content()
     input("Press Enter to continue...")
     m.show_data()
     input("Press Enter to continue...")
--- Scraper.py
+++ Scraper.py
@@ -14,13 +14,12 @@ Interfaz: reload_on(self,site); destroy(self); show_data(self); save_data(self);
 
 class Scraper:
     product_dict = {}
+    page_data = [] # [0,1] = Box; [2,3] = Description; [4,5] = Price
 
     def __init__(self, site, hide_gui: bool):
-        # Guardar el sitio.
-        self.actual_site = site
+        self.options = Options()
         if hide_gui:
             # Esconder GUI.
-            self.options = Options()
             self.options.add_argument("-headless")
         # Evitar la carga de imágenes.
         firefox_profile = webdriver.FirefoxProfile()
@@ -33,9 +32,8 @@ class Scraper:
         self.page = self.driver.page_source
 
     # PROPÓSITO: Recargar la instancia de la página en !site.
-    # COND: !page debe ser una url.
+    # PARAMS: !site representa una url.
     def reload_on(self, site):
-        self.actual_site = site
         self.driver.get(site)
         sleep(3)
         self.page = self.driver.page_source
@@ -56,26 +54,38 @@ class Scraper:
                 data_csv.writerow([key, self.product_dict[key]])
 
     # PROPÓSITO: Recibir la información de @page y guardar esa información en @product_dict.
-    # COND: 
-    # - !Box debe ser el *class name* de un contenedor (div) donde estén todos los productos.
-    # - !description debe ser el *class name* de los elementos que poseen la descripción del producto.
-    # - !price debe ser el *class name* de los elementos que poseen el precio del producto.
-    def get_content(self, box, description, price):
+    # COND: @page debe tener una página.
+    def get_content(self):
         soup = BeautifulSoup(self.page,'lxml')
-        content = soup.find_all('div', class_=box)
+        content = soup.find_all(self.page_data[1], class_=self.page_data[0])
         for product in content:
-            desc_prod = product.find('span', class_=description)
-            price_prod = product.find('div', class_=price)
+            desc_prod = product.find(self.page_data[3], class_=self.page_data[2])
+            price_prod = product.find(self.page_data[5], class_=self.page_data[4])
             # Agrega todas las descripciones nuevas junto con los precios de cada producto en @product_dict.
             if not(desc_prod.text in self.product_dict.keys()) or self.product_dict[desc_prod.text] > price_prod.text:
                 self.product_dict[desc_prod.text] = price_prod.text
 
+    # PROPÓSITO: Recibir toda la información relevante de la solapa donde @page está posicionada. 
+    # COND: @driver debe estar en una página con la información de *button_xpath*.
+    # PARAMS:
+    # - !button_xpath representa el *xpath* del botón para ir a la siguiente página.
+    # - !button_visual_xpath (OPCIONAL) representa el *xpath* de algún elemento previo para que !button_xpath sea visible.
+    def get_all(self, button_xpath, button_visual_xpath = None):
+        site = self.driver.current_url
+        self.get_content()
+        self.go_to_next_page(button_xpath, button_visual_xpath)
+        while site != self.driver.current_url :
+            sleep(3)
+            site = self.driver.current_url
+            self.get_content()
+            self.go_to_next_page(button_xpath, button_visual_xpath)
+            
     # PROPÓSITO: Buscar !search en la barra de busqueda !box.
-    # COND: 
-    # - !Box debe ser el *class name* de la barra de busqueda.
-    # - !search debe ser un string con lo que se quiere buscar.
-    def search_for(self, search, box):
-        search_bar = self.driver.find_element(By.CLASS_NAME, box)
+    # PARAMS: 
+    # - !Box representa el *class name* de la barra de busqueda.
+    # - !search representa un string de lo que se quiere buscar.
+    def search_for(self, search, searchbox_name):
+        search_bar = self.driver.find_element(By.CLASS_NAME, searchbox_name)
         search_bar.send_keys(search)
         search_bar.send_keys(Keys.ENTER)
         try:
@@ -85,13 +95,29 @@ class Scraper:
             print("Page took way to long to load ...")
 
     # PROPÓSITO: Describe si existe !box y sus elementos.
-    # COND: !Box debe ser el *class name* de un contenedor (div) donde estén todos los productos.
+    # PARAMS: !Box representa el *class name* de un contenedor (div) donde estén todos los productos.
     def check_if_data_exists(self, box):
         try:
             self.driver.find_element(By.CLASS_NAME, box)
         except NoSuchElementException:
             return False
         return True
+
+    # PROPÓSITO: Ir hasta la página siguiente haciendo click en el botón correspondiente.
+    # COND: @driver debe estar en una página con un boton para ir a la siguiente.
+    # PARAMS:
+    # - !button_xpath representa el *xpath* del botón para ir a la siguiente página.
+    # - !button_visual_xpath (OPCIONAL) representa el *xpath* de algún elemento previo para que !button_xpath sea visible.
+    def go_to_next_page(self, button_xpath: str, button_visual_xpath = None):
+        next_page = self.driver.find_element(By.XPATH, button_xpath)
+        if button_visual_xpath != None:
+            scroll_element = self.driver.find_element(By.XPATH, button_visual_xpath)
+            self.driver.execute_script("arguments[0].scrollIntoView();", scroll_element)
+        else:
+            self.driver.execute_script("arguments[0].scrollIntoView();", next_page)
+        sleep(1.5)
+        next_page.click()
+        sleep(1.5)
 
 # PROPÓSITO: borrar el csv.
 def delete_data():
